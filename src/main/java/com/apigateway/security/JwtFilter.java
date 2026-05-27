27a6ba364d20cc37e0229f0e21983c782963c0a3
praveen08das <106421248+praveen08das@users.noreply.github.com>
2026-05-27 14:20:08 +0530

    Api

---
+++ src/main/java/com/apigateway/security/JwtFilter.java
@@ -0,0 +1,83 @@
+package com.apigateway.security;
+
+import io.jsonwebtoken.Claims;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+
+import org.springframework.core.Ordered;
+
+import org.springframework.http.HttpHeaders;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+
+@Component
+public class JwtFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(
+            ServerWebExchange exchange,
+            GatewayFilterChain chain) {
+
+        String path =
+                exchange.getRequest()
+                        .getURI()
+                        .getPath();
+
+        // PUBLIC APIs
+        if (path.startsWith("/auth")) {
+
+            return chain.filter(exchange);
+        }
+
+        // HEADER
+        String authHeader =
+                exchange.getRequest()
+                        .getHeaders()
+                        .getFirst(HttpHeaders.AUTHORIZATION);
+
+        // TOKEN CHECK
+        if (authHeader == null ||
+                !authHeader.startsWith("Bearer ")) {
+
+            exchange.getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+
+            return exchange.getResponse().setComplete();
+        }
+
+        String token =
+                authHeader.substring(7);
+
+        try {
+
+            jwtUtil.extractUsername(token);
+
+        } catch (Exception e) {
+
+            exchange.getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+
+        return -1;
+    }
+}
