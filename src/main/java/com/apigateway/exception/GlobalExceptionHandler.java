b990224f8d3ce65880a2584763cc36bf636fedfb
praveen08das <106421248+praveen08das@users.noreply.github.com>
2026-06-09 17:55:19 +0530

    My changes

---
+++ src/main/java/com/apigateway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.apigateway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+}
--- src/main/java/com/apigateway/security/JwtFilter.java
+++ src/main/java/com/apigateway/security/JwtFilter.java
@@ -1,26 +1,21 @@
 package com.apigateway.security;
 
-import io.jsonwebtoken.Claims;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-
 import org.springframework.core.Ordered;
-
 import org.springframework.http.HttpHeaders;
-
 import org.springframework.http.HttpStatus;
-
 import org.springframework.stereotype.Component;
-
 import org.springframework.web.server.ServerWebExchange;
-
 import reactor.core.publisher.Mono;
-
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 
 @Component
 public class JwtFilter implements GlobalFilter, Ordered {
+
+    // SECRET KEY
+    private final String SECRET =
+            "mysecretkeymysecretkeymysecretkey";
 
     @Autowired
     private JwtUtil jwtUtil;
